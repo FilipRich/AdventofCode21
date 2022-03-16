9c52dadaa0ceb4916e08457679b3314d6c36aeb8
FilipRich <filip.richeberg@hotmail.com>
2022-03-16 22:09:53 +0100

    Update day3.py

--- day3/day3.py
+++ day3/day3.py
@@ -1,3 +1,6 @@
+from math import gamma
+
+
 data = """00100
 11110
 10110
@@ -10,4 +13,28 @@ data = """00100
 11001
 00010
 01010"""
+
+
+mylist = data.split("\n")
+check_list = []
+gamma_rate = ""
+epsilon_rate = ""
+bit = 0
+while bit < 5:
+    for i, x in enumerate(mylist):
+        check_list.append(int(x[bit]))
+    if sum(check_list) > (len(mylist)*0.5):
+        gamma_rate += "1"
+        epsilon_rate += "0"
+    else:
+        gamma_rate += "0"
+        epsilon_rate += "1"
+    bit += 1
+    check_list.clear()
+
+
+
+print(gamma_rate)
+print(epsilon_rate)
+
 
