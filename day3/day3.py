bbda6834c74e3c7a34a391c9c750c32cf2e7402e
FilipRich <filip.richeberg@hotmail.com>
2022-04-04 12:49:43 +0200

    Update day3.py

--- day3/day3.py
+++ day3/day3.py
@@ -39,31 +39,18 @@ def part1():
     print(int(gamma_rate, 2) * int(epsilon_rate, 2))
 
 def part2():
-    mylist = data.split("\n")
-    check_list = []
-    gamma_rate = []
-    epsilon_rate = []
-    bit = 0
-    while bit < len(mylist[0]):
-        for i, x in enumerate(mylist):
-            check_list.append(int(x[bit]))
-        if sum(check_list) >= (len(mylist)*0.5):
-            for i in mylist:
-                if i[bit] == "1":
-                    gamma_rate.append(i)
-                else:
-                    epsilon_rate.append
-                gamma_rate += "1"
-                epsilon_rate += "0"
-        else:
-            gamma_rate += "0"
-            epsilon_rate += "1"
-        bit += 1
-        check_list.clear()
+    mylist = my_file.read().split("\n")
+    oxygen_generator_rating(mylist)
+    co2_scrubber_rating(mylist)
+
+    #oxygen_gen = oxygen_gen_list.pop()
+    #co2_scubber = co2_scruber_list.pop()
+
+    #print(oxygen_gen + " " + co2_scubber)
 
 
-def oxygen_generator_rating():
-    mylist = data.split("\n")
+def oxygen_generator_rating(file):
+    mylist = file
     check_list = []
     temp_list = []
     o_gen = []
@@ -71,19 +58,42 @@ def oxygen_generator_rating():
     while bit < len(mylist[0]):
         if len(mylist) != 1:
             for i in mylist:
-                print(f"round {bit}: {i}")
                 check_list.append(int(i[bit]))
             if sum(check_list) >= (len(mylist)*0.5):
-                for i in mylist:
-                    if "0" == i[bit]:
-                        mylist.remove(i)
+                for i in mylist[:]:
+                    if i[bit] == "0":
+                        print(f"oxygen removing {i}")
+                        mylist.remove(i)        
             else:
-                for i in mylist:
-                    if "1" == i[bit]:
+                for i in mylist[:]:
+                    if i[bit] == "1":
                         mylist.remove(i)
             bit += 1
             check_list.clear()
     print(mylist)
 
+def co2_scrubber_rating(file):
+    print("Hello")
+    mylist = file
+    check_list = []
+    temp_list = []
+    o_gen = []
+    bit = 0
+    while (bit < len(mylist[0])) and (len(mylist) != 1):
+        for i in mylist:
+            check_list.append(int(i[bit]))
+            print(f"round {bit} val: {i}")
+        if sum(check_list) >= (len(mylist)*0.5):
+            for i in mylist[:]:
+                if i[bit] == "1":
+                    print(f"scrubber Removing {i}")
+                    mylist.remove(i)
+        else:
+            for i in mylist[:]:
+                if i[bit] == "0":
+                    mylist.remove(i)
+        bit += 1
+        check_list.clear()
+    print(mylist)
 
-oxygen_generator_rating()
+part2()
